c2a715f973e7085b7a748318def53a1e4b77e0f4
BillyLop <arthurboy.lopez@gmail.com>
2018-04-17 20:00:11 -0600

    Version con personajes y carros

---
+++ Project2_crossroad/Carro.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2_crossroad
+{
+    class Carro : Pieza
+    {
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+
+        public Carro (string nombre, int posX, int posY)
+        {
+            this.nombre = nombre;
+            this.tipo = "Carro";
+            this.posX = posX;
+            this.posY = posY;
+            if (nombre == "Azul")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/azul.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+            if (nombre == "Azulregreso")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/azulregreso.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+            if (nombre == "Amarillo")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/amarillo.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+
+            if (nombre == "Amarilloregreso")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/amarilloregreso.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+            if (nombre == "Rojo")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/rojo.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+
+            if (nombre == "Rojoregreso")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/rojoregreso.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+            if (nombre == "Verde")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/verde.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+
+            if (nombre == "Verderegreso")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/verderegreso.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+            if (nombre == "Negro")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/negro.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+
+            if (nombre == "Negroregreso")
+            {
+                figura = new PictureBox();
+                figura.Image = Image.FromFile("../../imagenes/negroregreso.png");
+                figura.Size = new Size(60, 50);
+                figura.SizeMode = PictureBoxSizeMode.StretchImage;
+                figura.BackColor = Color.Transparent;
+            }
+        }
+
+        public void avanzar()
+        {
+            if (direccion == "Izquierda")
+            {
+                if (posX == 0)
+                {
+                    posX = 9;
+                }
+                else
+                {
+                    posX = posX - 1;
+                }
+            }
+            else
+            {
+                if (posX == 9)
+                {
+                    posX = 0;
+                }
+                else
+                {
+                    posX = posX + 1;
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ Project2_crossroad/CrossRoad.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2_crossroad
+{
+    public partial class CrossRoad : Form
+    {
+        Tablero tablero;
+        
+        public void CreaTablero()
+        {
+
+            tablero = new Tablero(12, 10);
+            tablero.piezas = new List<Pieza>();
+            //llenar tablero
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    Pieza pieza = new Pieza("Pasto", x, y);
+                    tablero.piezas.Add(pieza);
+                }
+            }
+
+            for (int x = 0; x < 10; x++)
+            {
+                Pieza pieza = new Pieza("Carretera", x, 2);
+                tablero.piezas.Add(pieza);
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                Pieza pieza = new Pieza("Carreteraabajo", x, 3);
+                tablero.piezas.Add(pieza);
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 4; y < 6; y++)
+                {
+                    Pieza pieza = new Pieza("Pasto", x, y);
+                    tablero.piezas.Add(pieza);
+                }
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                Pieza pieza = new Pieza("Carretera", x, 6);
+                tablero.piezas.Add(pieza);
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                Pieza pieza = new Pieza("Carreteraabajo", x, 7);
+                tablero.piezas.Add(pieza);
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 8; y < 10; y++)
+                {
+                    Pieza pieza = new Pieza("Pasto", x, y);
+                    tablero.piezas.Add(pieza);
+                }
+            }
+
+            tablero.personaje = new Personaje("Kirito", 5, 9);
+            
+
+            tablero.carros = new List<Carro>();
+            Carro carro = new Carro("Azul", 9, 2);
+            carro.direccion = "Izquierda";
+            tablero.carros.Add(carro);
+
+            Carro carro1 = new Carro("Azulregreso", 0, 3);
+            carro1.direccion = "Derecha";
+            tablero.carros.Add(carro1);
+
+            Carro carro2 = new Carro("Amarillo", 7, 2);
+            carro2.direccion = "Izquierda";
+            tablero.carros.Add(carro2);
+
+            Carro carro3 = new Carro("Amarilloregreso", 3, 3);
+            carro3.direccion = "Derecha";
+            tablero.carros.Add(carro3);
+
+            Carro carro4 = new Carro("Rojo", 9, 6);
+            carro4.direccion = "Izquierda";
+            tablero.carros.Add(carro4);
+
+            Carro carro5 = new Carro("Rojoregreso", 0, 7);
+            carro5.direccion = "Derecha";
+            tablero.carros.Add(carro5);
+
+            Carro carro6 = new Carro("Verde", 5, 6);
+            carro6.direccion = "Izquierda";
+            tablero.carros.Add(carro6);
+
+            Carro carro7 = new Carro("Verderegreso", 3, 7);
+            carro7.direccion = "Derecha";
+            tablero.carros.Add(carro7);
+
+            Carro carro8 = new Carro("Negro", 2, 6);
+            carro8.direccion = "Izquierda";
+            tablero.carros.Add(carro8);
+
+            Carro carro9 = new Carro("Negroregreso", 5, 7);
+            carro9.direccion = "Derecha";
+            tablero.carros.Add(carro9);
+
+            int posX;
+            int posY;
+            //dibujar tablero
+            foreach (Pieza pieza in tablero.piezas)
+            {
+                posX = 10 + (pieza.posX * 60);
+                posY = 40 + (pieza.posY * 50);
+                pieza.figura.Location = new Point(posX, posY);
+                this.Controls.Add(pieza.figura);
+            }
+            
+            //dibujar personaje
+            posX = 10 + (tablero.personaje.posX * 60);
+            posY = 40 + (tablero.personaje.posY * 50);
+            tablero.personaje.figura.Location = new Point(posX, posY);
+            this.Controls.Add(tablero.personaje.figura);
+            tablero.personaje.figura.BackColor = Color.Transparent;
+            tablero.personaje.figura.BringToFront();
+
+            //dibujar carros
+            foreach (Carro carron in tablero.carros)
+            {
+                posX = 10 + (carron.posX * 60);
+                posY = 40 + (carron.posY * 50);
+                carron.figura.Location = new Point(posX, posY);
+                this.Controls.Add(carron.figura);
+                carron.figura.BackColor = Color.Transparent;
+                carron.figura.BringToFront();
+            }
+        }
+        public CrossRoad()
+        {
+            InitializeComponent();
+            CreaTablero();
+
+        }
+        private void dibujarCarro(Carro carro)
+        {
+            int posX = 10 + (carro.posX * 60);
+            int posY = 40 + (carro.posY * 50);
+            carro.figura.Location = new Point(posX, posY);
+            carro.figura.BackColor = Color.Transparent;
+            carro.figura.BringToFront();
+        } 
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            tablero.personaje.moverAdelante();
+            dibujarPersonaje(tablero.personaje.posX, tablero.personaje.posY);
+            personajeAtropellado();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            tablero.personaje.moverIzquierda();
+            dibujarPersonaje(tablero.personaje.posX, tablero.personaje.posY);
+            personajeAtropellado();
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            tablero.personaje.moverDerecha();
+            dibujarPersonaje(tablero.personaje.posX, tablero.personaje.posY);
+            personajeAtropellado();
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            tablero.personaje.moverAtras();
+            dibujarPersonaje(tablero.personaje.posX, tablero.personaje.posY);
+            personajeAtropellado();
+        }
+        private void dibujarPersonaje(int x, int y)
+        {
+            int posX = 10 + (x * 60);
+            int posY = 40 + (y * 50);
+            tablero.personaje.figura.Location = new Point(posX, posY);
+            tablero.personaje.figura.BackColor = Color.Transparent;
+            tablero.personaje.figura.BringToFront();
+        }
+
+
+        void avanzarCarroAsync(Carro carro)
+        {
+              for (int x = 0; x < 2; x++)
+            {
+                carro.avanzar();
+                dibujarCarro(carro);
+
+            }
+        }
+        private void timerCarro1_Tick(object sender, EventArgs e)
+        {
+            foreach (Carro carro in tablero.carros)
+            {
+                avanzarCarroAsync(carro);
+                personajeAtropellado();
+            }
+        }
+
+        private bool personajeAtropellado()
+        {
+            if (tablero.carros.Any(carro => carro.posX == tablero.personaje.posX && carro.posY == tablero.personaje.posY))
+            {
+                MessageBox.Show("Game Over, Fuiste atropellado");
+                return true;
+            }
+            return false;
+        }
+    }
+}
